b8152b2c00d5aaaf80df5502e1027de8fe88e9b0
Matheus Pavanelli <mspavanelli@gmail.com>
2019-12-03 17:50:42 -0300

    Add Home styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,98 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 const Home = () => (
-  <div>
-    <h1>Loja</h1>
-  </div>
+  <ProductList>
+    <li>
+      <img
+        src="https://dks.scene7.com/is/image/dkscdn/19NIKMDWNSHFTR9RDRNN_Black_White_is?wid=1080&fmt=jpg"
+        alt="Tênis"
+      />
+      <strong>Tênis</strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" />1
+        </div>
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </li>
+    <li>
+      <img
+        src="https://dks.scene7.com/is/image/dkscdn/19NIKMDWNSHFTR9RDRNN_Black_White_is?wid=1080&fmt=jpg"
+        alt="Tênis"
+      />
+      <strong>
+        Tênis com descrição tão grande que quebra em mais de uma linha
+      </strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" />1
+        </div>
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </li>
+    <li>
+      <img
+        src="https://dks.scene7.com/is/image/dkscdn/19NIKMDWNSHFTR9RDRNN_Black_White_is?wid=1080&fmt=jpg"
+        alt="Tênis"
+      />
+      <strong>Tênis</strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" />1
+        </div>
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </li>
+    <li>
+      <img
+        src="https://dks.scene7.com/is/image/dkscdn/19NIKMDWNSHFTR9RDRNN_Black_White_is?wid=1080&fmt=jpg"
+        alt="Tênis"
+      />
+      <strong>Tênis</strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" />1
+        </div>
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </li>
+    <li>
+      <img
+        src="https://dks.scene7.com/is/image/dkscdn/19NIKMDWNSHFTR9RDRNN_Black_White_is?wid=1080&fmt=jpg"
+        alt="Tênis"
+      />
+      <strong>Tênis</strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" />1
+        </div>
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </li>
+    <li>
+      <img
+        src="https://dks.scene7.com/is/image/dkscdn/19NIKMDWNSHFTR9RDRNN_Black_White_is?wid=1080&fmt=jpg"
+        alt="Tênis"
+      />
+      <strong>Tênis</strong>
+      <span>R$129,90</span>
+      <button type="button">
+        <div>
+          <MdAddShoppingCart size={16} color="#fff" />1
+        </div>
+        <span>Adicionar ao carrinho</span>
+      </button>
+    </li>
+  </ProductList>
 );
 
 export default Home;
